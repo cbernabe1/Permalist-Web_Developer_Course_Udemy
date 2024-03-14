748d82acecacf499a14427cec214646b3c7fef83
cbernabe1 <bernabealan05@gmail.com>
2024-03-14 15:22:39 +0800

    Second Commit

--- index.js
+++ index.js
@@ -8,30 +8,64 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "pg123",
+  port: 5432
+});
+db.connect();
 
 let items = [];
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+let currentDate = day + "-" + month + "-" + year;
+let filterList = ['Today','Yesterday', 'All'];
+let numberOfTimes = 0;
+let dateToFetch;
+const yesterday = () => {
+  let d = new Date();
+  d.setDate(d.getDate() - 1);
+  return d;
+};
 
-
-
+if(filterList[numberOfTimes] === 'Today'){
+  dateToFetch = currentDate;
+}else if(filterList[numberOfTimes] === 'Yesterday'){
+  dateToFetch = yesterday().toISOString().split('T')[0];
+}
 app.get("/", async(req, res) => {
   items = [];
-  const result = await db.query("SELECT * FROM items");
+  if(filterList !== 'All'){
+  const result = await db.query("SELECT * FROM items WHERE creation_date = $1",[dateToFetch]);
   result.rows.forEach((item) =>{
     items.push(item);
   });
+  }else{
+    const result = await db.query("SELECT * FROM items ORDER BY creation_date");
+    result.rows.forEach((item) =>{
+      items.push(item);
+    });
+  }
   res.render("index.ejs", {
-    listTitle: "Today",
+    listTitle: filterList[numberOfTimes],
     listItems: items,
   });
   console.log(items);
+  numberOfTimes++;
+  if(numberOfTimes === filterList.length){
+    numberOfTimes = 0;
+  }
 });
 
 app.post("/add", async(req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  const type = req.body.newType;
   try {
-    await db.query("INSERT INTO items (name) VALUES ($1)",[item]);
+    await db.query("INSERT INTO items (name,creation_date,type) VALUES ($1,$2,$3)",[item,currentDate,type]);
     res.redirect("/");
   } catch (error) {
     console.log(error);
